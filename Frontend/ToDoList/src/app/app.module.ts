3e4a16c81b3a62e27984a2f74394887563c610b9
Soufiane LAAROUSSI <soufianelaaroussi03@gmail.com>
2024-01-25 01:47:07 +0100

    Added Components to App Module File

--- Frontend/ToDoList/src/app/app.module.ts
+++ Frontend/ToDoList/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SigninComponent } from './auth/signin/signin.component';
+import { SignupComponent } from './auth/signup/signup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SigninComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
