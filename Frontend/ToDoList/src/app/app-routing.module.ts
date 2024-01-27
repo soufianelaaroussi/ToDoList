b80569d32ff7d364e7ed7483d14bc3d11f087dc9
Soufiane LAAROUSSI <soufianelaaroussi03@gmail.com>
2024-01-27 20:50:39 +0100

    Modified App And App Routing Modules

--- Frontend/ToDoList/src/app/app-routing.module.ts
+++ Frontend/ToDoList/src/app/app-routing.module.ts
@@ -1,10 +1,40 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SigninComponent } from './auth/signin/signin.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { AddtaskComponent } from './task/addtask/addtask.component';
+import { UpdatetaskComponent } from './task/updatetask/updatetask.component';
+import { MytaskslistComponent } from './task/mytaskslist/mytaskslist.component';
+import { TaskComponent } from './task/task.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'signin', component: SigninComponent },
+  { path: 'signup', component: SignupComponent },
+  {
+    path: 'task',
+    component: TaskComponent,
+    children: [
+      {
+        path: 'addtask',
+        title: 'Add A Task',
+        component: AddtaskComponent,
+      },
+      {
+        path: 'updatetask',
+        title: 'Update Task',
+        component: UpdatetaskComponent,
+      },
+      {
+        path: 'taskslist',
+        title: 'My Tasks List',
+        component: MytaskslistComponent,
+      },
+    ],
+  },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes, { enableTracing: true })],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- Frontend/ToDoList/src/app/app.module.ts
+++ Frontend/ToDoList/src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SigninComponent } from './auth/signin/signin.component';
 import { SignupComponent } from './auth/signup/signup.component';
+import { SigninComponent } from './auth/signin/signin.component';
+import { AddtaskComponent } from './task/addtask/addtask.component';
+import { UpdatetaskComponent } from './task/updatetask/updatetask.component';
+import { MytaskslistComponent } from './task/mytaskslist/mytaskslist.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    SignupComponent,
     SigninComponent,
-    SignupComponent
+    AddtaskComponent,
+    UpdatetaskComponent,
+    MytaskslistComponent
   ],
   imports: [
     BrowserModule,
